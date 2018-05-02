621d964d215ceba8017e75add49d2df9c3bbf86a
Jonathan Bieler <JBieler@sophiagenetics.com>
2018-05-02 16:20:17 +0200

    B,D version working

--- src/CMAES.jl
+++ src/CMAES.jl
@@ -1,5 +1,4 @@
 module CMAES
-
 ##
 
     using Optim, Distributions, Parameters, PDMats
@@ -7,7 +6,6 @@ module CMAES
                   trace!, assess_convergence, AbstractOptimizerState, update!, value, value!,
                   pick_best_x, pick_best_f, f_abschange, x_abschange
 
-    #diagonal doesn't work on v0.6, but might in 0.7
     const MatTypes = Union{Matrix,Diagonal}
     
     @static if VERSION < v"0.7.0-DEV.3510"
@@ -47,17 +45,18 @@ module CMAES
         f_x_previous::T
         #
         w::Vector{T}
-        D::Int
+        n::Int
         μ_w::T
         c_σ::T
         d_σ::T
         c_c::T
-        c_1::T
-        c_μ::T
+        c_cov::T
         #
         p_σ::Vector{T}
         p_c::Vector{T}
         C::K
+        D::Vector{T}
+        B::K
         m::Vector{T}
         chi_D::T
         σ::T
@@ -72,32 +71,33 @@ module CMAES
     
     ##
 
-    function init_C(method::CMA{K},D) where K <: MatTypes
-        K <: Matrix ? diagm(ones(D)) : Diagonal(ones(D))
+    function init_matrix(method::CMA{K},n) where K <: MatTypes
+        K <: Matrix ? diagm(ones(n)) : Diagonal(ones(n))
     end
 
     function initial_state(method::CMA, options, d, xinit) 
         
         λ = method.λ
-        D = length(xinit)
+        n = length(xinit)
         
         μ = floor(Int,λ/2)
         w = weights(μ)
         
-        D, μ_w, c_σ, d_σ, c_c, c_1, c_μ = init_constants(method,xinit,λ,w,μ)
-        p_σ, p_c = zeros(D), zeros(D)
-        C = init_C(method,D)
+        n, μ_w, c_σ, d_σ, c_c, c_cov = init_constants(method,xinit,λ,w,μ)
+        p_σ, p_c = zeros(n), zeros(n)
+        C,B = init_matrix(method,n), init_matrix(method,n)
+        D = ones(n)
         
         m = xinit
     
-        chi_D = √D*(1-1/(4*D) + 1/(21*D^2))
-        xs = [zeros(D) for i=1:μ]
+        chi_D = √n*(1-1/(4*n) + 1/(21*n^2))
+        xs = [zeros(n) for i=1:μ]
         σ_0 = copy(method.σ)
         
         CMAState(
             xinit,xinit,Inf,Inf,w,
-            D, μ_w, c_σ, d_σ, c_c, c_1, c_μ,
-            p_σ, p_c,C,m,chi_D,method.σ,σ_0,λ,1,xs
+            n, μ_w, c_σ, d_σ, c_c, c_cov,
+            p_σ, p_c,C,D,B,m,chi_D,method.σ,σ_0,λ,1,xs
         )
     end
     
@@ -118,12 +118,10 @@ module CMAES
         options.callback)
     end
     
-    mv_rand(D,C) = rand(MultivariateNormal(zeros(D),C))
-    mv_rand(D,C::Diagonal) = rand(MultivariateNormal(zeros(D),diag(C)))
-
-    function update_state!{T}(d, state::CMAState{T}, method::CMA)
+  
+    function update_state!(d, state::CMAState{T,K}, method::CMA) where {T,K}
     
-        @unpack w,D,μ_w,c_σ,d_σ,c_c,c_1,c_μ,p_σ,p_c,C,m,chi_D,σ,t,xs = state
+        @unpack w,n,μ_w,c_σ,d_σ,c_c,c_cov,p_σ,p_c,C,D,B,m,chi_D,σ,t,xs = state
         
         state.f_x_previous = state.f_x
         copy!(state.x_previous, state.x)
@@ -131,84 +129,90 @@ module CMAES
         λ = method.λ
         μ = floor(Int,λ/2)
 
-        x = [m + σ*mv_rand(D,C) for i=1:λ]
+        BD = B*Diagonal(D)
+
+        z = [randn(n) for i=1:λ]
+        x = [m + σ*BD*z[i] for i=1:λ]
     
         fx = zeros(T,λ)
         for i=1:λ
             value!(d,x[i])
             fx[i] = value(d)
-            isnan(fx[i]) && error("Function return NaN for input $(x[i])")
+            isnan(fx[i]) && error("Function returned NaN for input $(x[i])")
         end
 
         idx = sortperm(fx)  
-        x, fx = x[idx], fx[idx]
+        x,z,fx = x[idx], z[idx], fx[idx]
         
-        m_t = m
-        m = ∑( w[i] * x[i] for i=1:μ)
+        m = ∑( w[i] * x[i] for i=1:μ )
+        ẑ = ∑( w[i] * z[i] for i=1:μ )
         
-        Δm = m-m_t
+        p_σ = (1-c_σ)*p_σ  + √(c_σ*(2-c_σ)*μ_w)*B*ẑ
         
-        p_σ = update_p_σ(c_σ, p_σ, μ_w, C, Δm, σ)
+        H_σ = norm(p_σ) < √(1-(1-c_σ)^(2*(t+1)))*(1.4 + 2/(n+1)) ? 1.0 : 0.0
         
-        h_σ = norm(p_σ) < √(1-(1-c_σ)^(2*(t+1)))*(1.4 + 2/(D+1)) ? 1.0 : 0.0
-        
-        p_c = (1-c_c)*p_c + h_σ*√(c_σ*(2 - c_σ)*μ_w) * Δm/σ
+        p_c = (1-c_c)*p_c + H_σ*√(c_σ*(2 - c_σ)*μ_w)*BD*ẑ
            
-        C = update_C(C, c_1, c_μ, h_σ, c_c, p_c, w, σ, x, m, μ)
+        C = update_C(C,c_cov, μ_w, p_c, w, BD, z, μ)
         
         σ = σ * exp(c_σ/d_σ*(norm(p_σ)/chi_D -1))
             
-        @show p_σ
-            
+        D,B = eig(C)
+        B = K <: Diagonal ? Diagonal(B) : B
+#        @show D
+        D = sqrt.(D) 
+        
         t += 1
             
         state.x = x[1]
         state.f_x = fx[1]
         xs = x
 
-        @pack state = w,D,μ_w,c_σ,d_σ,c_c,c_1,c_μ,p_σ,p_c,C,m,chi_D,σ,t,xs
+        @pack state = w,n,μ_w,c_σ,d_σ,c_c,c_cov,p_σ,p_c,C,D,B,m,chi_D,σ,t,xs
                 
         false # should the procedure force quit?
     end
     
-    function update_C(C::Matrix, c_1, c_μ, h_σ, c_c, p_c, w, σ, x, m, μ)
     
-        C = (1-c_1-c_µ + (1-h_σ)*c_1*c_c*(2-c_c)) * C +
-            c_1 * p_c * p_c' +
-            c_μ * ∑( w[i]/σ^2*( (x[i]-m)*(x[i]-m)') for i=1:μ)
-            
-        C = (C+C')/2 #keep symmetric part
+    ⊗(::Type{Matrix{T}},x)   where T = x * x'
+    ⊗(::Type{Diagonal{T}},x) where T = Diagonal(x.^2)
+    
+    function update_C(C::K, c_cov, μ_w, p_c, w, BD, z, μ) where K <: MatTypes
+        C = (1-c_cov) * C +
+            c_cov * 1/μ_w * ⊗(K,p_c) +
+            c_cov * (1-1/μ_w) * ∑( w[i]* ⊗(K,BD*z[i]) for i=1:μ)
         C
     end
+    
+    function init_constants(method::CMA{K}, xinit,λ,w,μ) where K <: MatTypes
+        n = length(xinit)
+    
+        μ_w = 1.0 / sum(w.^2)
+        c_σ =  (μ_w + 2.0) / (n + μ_w + 5.0)
         
-    #this is probably wrong
-    function update_C(C::Diagonal, c_1, c_μ, h_σ, c_c, p_c, w, σ, x, m, μ)
-    
-        C = (1-c_1-c_µ + (1-h_σ)*c_1*c_c*(2-c_c)) * C +
-            c_1 * Diagonal(p_c.^2) +
-            c_μ * ∑( w[i]/σ^2*( Diagonal((x[i]-m).^2) ) for i=1:μ)
-            
-        C
+        d_σ = 1.0 + c_σ + 2.0*max(0, √((μ_w-1)/(n+1)) -1)
+        
+        c_c = (4 + μ_w/n)/(n + 4 + 2μ_w/n) #Benchmarking a BI-Population CMA-ES on the BBOB-2009 Function Testbed
+        
+        c_cov = 1/μ_w*2/((n+1.3)^2) + (1-1/μ_w)*min(1,2*(μ_w-1)/((n+2)^2 +μ_w)) 
+         
+        if K <: Diagonal #A Simple Modification in CMA-ES Achieving Linear Time and Space Complexity
+            c_cov = min(1, c_cov*(n+2)/3) # I need to clamp here for low n
+        end
+        
+        n, μ_w, c_σ, d_σ, c_c, c_cov
     end
-    
-    function update_p_σ(c_σ, p_σ, μ_w, C, Δm, σ)
-        (1-c_σ)*p_σ  + √(c_σ*(2-c_σ)*μ_w) * C^(-1/2) * Δm/σ
-    end
-    
-    function update_p_σ(c_σ, p_σ, μ_w, C::Diagonal, Δm, σ)
-        (1-c_σ)*p_σ  + √(c_σ*(2-c_σ)*μ_w) * C^(-1/2) * Δm/σ
-    end
-    
+
     function assess_convergence(state::CMAState, d, options)
         
         x_converged, f_converged, g_converged, converged, f_increased = Optim.default_convergence_assessment(state, d, options)
         f_increased = false #disable this one, since error can increase with CMAES
 
-        @unpack D, λ, p_c, σ, σ_0,t,C = state
+        @unpack n, λ, p_c, σ, σ_0,t,C = state
 
         d,_ = eig(C) #FIXME maybe a bit costly
 
-        MaxIter = t > 100 + 50*(D+3)*2/√(λ)
+        MaxIter = t > 100 + 50*(n+3)*2/√(λ)
 
         TolX = t > 1 && all(p_c * σ/σ_0 .< 1e-16) && all(sqrt.(diag(C)) * σ/σ_0  .< 1e-16)
         TolUpSigma = σ/σ_0 > 10^20 * √(maximum(d))
@@ -224,29 +228,6 @@ module CMAES
 
     pick_best_f(f_increased, state::CMAState, d) = state.f_x 
     pick_best_x(f_increased, state::CMAState) = state.x
-
-    function init_constants(method::CMA{K}, xinit,λ,w,μ) where K <: MatTypes
-        D = length(xinit)
-    
-        μ_w = 1.0 / sum(w.^2)
-        c_σ =  (μ_w + 2.0) / (D + μ_w + 5.0)
-        
-        d_σ = 1.0 + c_σ + 2.0*max(0, √((μ_w-1)/(D+1)) -1) 
-        
-        c_c = (4 + μ_w/D)/(D + 4 + 2μ_w/D) 
-        
-        c_1 = 2/((D+1.3)^2 + μ_w)
-        c_μ = min(1-c_1,2*(μ_w-2+1/μ_w)/((D+2)^2 +μ_w))
-        
-        if K == Diagonal #A Simple Modification in CMA-ES Achieving Linear Time and Space Complexity
-#            d_σ *= 100#1.0 + c_σ + 2.0*max(0, √((μ_w-1)/(D+1)) -1) 
-#            
-#            c_1 = (D+2)/3 * 1/μ_w * 2/(√2 + D)^2
-#            c_μ = (D+2)/3 * (1-1/μ_w)*min(1,(2μ_w-1)/((D+2)^2 +μ_w))
-        end
-
-        D, μ_w, c_σ, d_σ, c_c, c_1, c_μ
-    end
    
 ##
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,23 +7,26 @@ using PDMats
 
 using BlackBoxOptimizationBenchmarking
 const BBOB = BlackBoxOptimizationBenchmarking
-f = BBOB.F2
+f = enumerate(BBOBFunction)[6]
 
 include("/Users/jbieler/.julia/v0.6/CMAES/src/CMAES.jl")
 
 mfit = optimize(
-   f,5*rand(2),CMAES.CMA(16,5),
-   Optim.Options(iterations=20,store_trace=true,extended_trace=true),
+   f,[-5.;4.],CMAES.CMA(8,1),
+   Optim.Options(iterations=2000,store_trace=true,extended_trace=true),
 )
+
+z = [mfit.trace[t].value for t=1:length(mfit.trace)]
+display(plot(y=z-f.f_opt+1e-16,yintercept=[1e-6],Geom.line,Geom.hline(color=colorant"gray"),Scale.y_log10))
 
 @assert Optim.minimum(mfit) < f.f_opt + 1e-6
 Optim.minimizer(mfit), f.x_opt[1:3]
 
-##
+## Diagonal one
 
 mfit = optimize(
-   f,5*rand(2),CMAES.CMA{Diagonal}(116,5),
-   Optim.Options(iterations=20,store_trace=true,extended_trace=true),
+   f,[-5.;4.],CMAES.CMA{Diagonal}(4,1),
+   Optim.Options(iterations=2000,store_trace=true,extended_trace=true),
 )
 
 #@assert Optim.minimum(mfit) < f.f_opt + 1e-6
@@ -33,20 +36,23 @@ x = [mean(mfit.trace[t].metadata["x"])[1] for t=1:length(mfit.trace)]
 y = [mean(mfit.trace[t].metadata["x"])[2] for t=1:length(mfit.trace)]
 z = [mfit.trace[t].value for t=1:length(mfit.trace)]
 
-plot(y=z-f.f_opt,yintercept=[1e-6],Geom.line,Geom.hline(color=colorant"gray"),Scale.y_log10)
+plot(y=z-f.f_opt+1e-16,yintercept=[1e-6],Geom.line,Geom.hline(color=colorant"gray"),Scale.y_log10)
 
 ##
 
     using Gadfly
-    fl = layer(z=(x,y) -> f([x;y])-f.f_opt, x=linspace(-5,5,200), y=linspace(-5,5,200), Geom.contour(levels=logspace(-8,8,15)))
+    fl = layer(z=(x,y) -> f([x;y])-f.f_opt, x=linspace(-6,6,400), y=linspace(-6,6,400), Geom.contour(levels=logspace(-4,8,20)))
 #    fl = layer(z=(x,y) -> f([x;y])-f.f_opt, x=linspace(-5,5,100), y=linspace(-5,5,100), Geom.contour())
 
     x = [mean(mfit.trace[t].metadata["x"])[1] for t=1:length(mfit.trace)]
     y = [mean(mfit.trace[t].metadata["x"])[2] for t=1:length(mfit.trace)]
 
-    plot(fl, 
-        layer(x=x,y=y,Geom.line, Theme(default_color=colorant"black") ), 
-        layer(x=[f.x_opt[1]],y=[f.x_opt[2]],Geom.point,Theme(default_color=colorant"red")) 
+    plot(
+        layer(x=[f.x_opt[1]],y=[f.x_opt[2]],Geom.point,Theme(default_color=colorant"red")), 
+        layer(x=x,y=y,Geom.line(preserve_order=true), Theme(default_color=colorant"black") ),
+        fl, 
+        Scale.color_continuous(colormap=p->Colors.RGB(p^0.4,p/2+0.4,p/3+0.4)),
+        Coord.cartesian(xmin=-6,xmax=6,ymin=-5,ymax=6),
     )
  
 ##
@@ -77,7 +83,6 @@ mfit = optimize(
 ##
 
 #include(joinpath(Pkg.dir(),"BlackBoxOptimizationBenchmarking","scripts","optimizers_interface.jl"))
-if false
 
 import BlackBoxOptimizationBenchmarking: minimizer, minimum, optimize
 import Base.string
@@ -86,9 +91,11 @@ pinit(D) = 10*rand(D)-5
 optimize(opt::Optim.AbstractOptimizer,f,D,run_length) =
 Optim.optimize(f, pinit(D), opt, Optim.Options(f_calls_limit=run_length,g_tol=1e-12))
 
-mean_succ, mean_dist, mean_fmin, runtime = BBOB.benchmark([CMAES.CMA(64,5)], [1 2 3 4 5], [20000], 20, 3, 1e-6)
+mean_succ, mean_dist, mean_fmin, runtime = BBOB.benchmark([CMAES.CMA(16,5)], [1 2 3 4 5], [20000], 20, 3, 1e-6)
 
 println(mean_succ)
 
-end
+
+
+##
 
